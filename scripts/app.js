0628bb29b4bbe2cd2988d9a5e6ebbddb27d4ffca
Mike Baker <bakermg@me.com>
2016-07-18 23:14:32 -0400

    feat: added search bar function url for api call and knockout observables

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,22 @@
-function initMap() {
+ function ViewModel() {
+  var self = this;
+  var map, city, infowindow;
+
+  this.activeEvents = ko.observable([]); //List of events
+  this.mapMarkers = ko.observable([]); //All Map Marker
+  this.filterlist = ko.observable([]); //Filtered list
+  this.numberOfEvents = ko.computed(function() {
+    return self.filterlist().length;
+  });
+
+
+//Error handling if Google Maps fails to load
+  this.mapRequestTimeout = setTimeout(function() {
+    $('#map-canvas').html('Google Maps failed to load Please refresh your browser.');
+  }, 8000);
+
+// Initialize Google map
+ function initMap() {
 
     // Some custom styling for the map
     var styles = [{
@@ -62,43 +80,35 @@ function initMap() {
         "featureType": "road"
     }];
 
-    //Create a new map
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: 26.09951,
-            lng: -80.38377
-        },
-        zoom: 12,
+
+     city = new google.maps.LatLng(26.09951, -80.38377);
+     map = new google.maps.Map(document.getElementById('map'), {
+        center: city,
+        zoom: 11,
         styles: styles,
-        mapTypeControl: false,
-    }
+        mapTypeControl: false
+    });
 
-  );
+      clearTimeout(self.mapRequestTimeout);
 
-  google.maps.event.addDomListener(window, "resize" , function() {
-    var center = map.getCenter();
-    google.maps.event.trigger(map, "resize");
-    map.setCenter(center);
-  });
+  // found stackoverflow trick to re-center map and make responsive
 
-    var largeInfowindow = new google.maps.InfoWindow({maxWidth: 300});
+    google.maps.event.addDomListener(window, "resize" , function() {
+      var center = map.getCenter();
+      google.maps.event.trigger(map, "resize");
+      map.setCenter(center);
+    });
 
-    /*var mapMarkers = new google.maps.Marker(
-      {
-        map: map,
-        position: position,
-        title: title,
-        icon: defaultIcon,
-        animation: google.maps.Animation.DROP,
-        id:  i
-      }
-    );*/
+     infowindow = new google.maps.InfoWindow({maxWidth: 300});
+
 }
 
-var ViewModel = function () {
-  var self = this;
+// Use API to get events data
+  function getEvents(location) {
+    var activeURL = "http://api.amp.active.com/v2/search?query=running&category=event&radius=50&zip="+zip+"&start_date="+date+"api_key=uq2yyhkfewq9j2te9j754g6g";
+  }
 
-  this.mapMarkers = ko.observable([]);
-  this.filterlist = ko.observable([]);
+  initMap();
 }
+
 ko.applyBindings(new ViewModel());
