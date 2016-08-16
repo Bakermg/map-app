1c0095b98650fa25fdb2e73fe8e1dd3edd06bdcd
Mike Baker <bakermg@me.com>
2016-08-15 22:20:24 -0400

    refactor: Changed the initial locations and added map makers to the map creating infowindows

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,15 @@
-function ViewModel() {
 
-    var self = this;
     var map, infowindow, title, pos;
     var myLatLng;
     var area;
     var city;
     var search;
 
-    this.topicTitles = ko.observable([]);
-    this.mapMarkers = ko.observable([]); //All Map Marker
-    this.topicLocation = ko.observable([]);
+    //this.topicTitles = ko.observable([]);
+    //this.mapMarkers = ko.observable([]); //All Map Marker
+    //this.topicLocation = ko.observable([]);
 
-    console.log(self.topicLocation);
-    console.log(self.topicTitles);
-    var markers = [];
+
     //Error handling if Google Maps fails to load
         this.mapRequestTimeout = setTimeout(function() {
             $('#map').html('Google Maps failed to load Please refresh your browser.');
@@ -111,9 +107,14 @@ function ViewModel() {
             mapTypeControl: false
         });
 
+         google.maps.event.addDomListener(window, "resize", function() {
+            var center = map.getCenter();
+            google.maps.event.trigger(map, "resize");
+            map.setCenter(center);
+        });
 
 
-        //var infoWindow = new google.maps.InfoWindow({map: map});
+        var infoWindow = new google.maps.InfoWindow({map: map});
 
         //Geolocation to find users local to center map
         /*if (navigator.geolocation) {
@@ -134,11 +135,7 @@ function ViewModel() {
           handleLocationError(false, infoWindow, map.getCenter());
         }
 
-        google.maps.event.addDomListener(window, "resize", function() {
-            var center = map.getCenter();
-            google.maps.event.trigger(map, "resize");
-            map.setCenter(center);
-        });
+
 
           clearTimeout(self.mapRequestTimeout);
 
@@ -156,20 +153,19 @@ function ViewModel() {
 
       var highlightedIcon = makeMarkerIcon('ffff24');
 
-      for (var i = 0; i < self.topicLocation.length; i++) {
-          var position = self.topiclocations[i].location;
+      for (var i = 0; i < initialLocations.length; i++) {
+          var position = initialLocations[i].location;
           //var title = topiclocations[i].title;
 
           var marker = new google.maps.Marker({
               map: map,
               position: position,
-              //title: title,
               icon: defaultIcon,
               animation: google.maps.Animation.DROP,
               id: i
           });
 
-          markers.push(marker);
+          this.markers.push(marker);
 
           marker.addListener('click', function() {
               populateInfoWindow(this, largeInfowindow);
@@ -194,7 +190,7 @@ function ViewModel() {
         //alert(city, search);
     //});
 
-    function getEvents(city, search) {
+    /*function getEvents(city, search) {
         var localURL = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20local.search%20where%20zip%3D'" + city + "'%20and%20query%3D'" + search + "'&format=json&diagnostics=true";
 
         $.ajax({
@@ -222,7 +218,7 @@ function ViewModel() {
               }
             });
 
-    }
+    }*/
 
     function makeMarkerIcon(markerColor) {
       var markerImage = new google.maps.MarkerImage(
@@ -234,13 +230,131 @@ function ViewModel() {
       return markerImage;
   }
 
-    console.log(this.topicTitles);
+
     //console.log(topicLocation);
-    initMap();
-    getEvents("33327", "running");
+
+    //getEvents("33327", "running");
+
+var initialLocations = [
+        {
+          name: "Fort Lauderdale Beach Park",
+          location: {
+            lat: 26.1120152,
+            lng: -80.1048193
+          },
+          streetAddress: "1100 Seabreaze Blvd",
+          city: "Fort Lauderdale",
+          zipcode: 33316,
+          cat: "park"
+        },
+        {
+          name: "Markham Park Moutain Bike Trails",
+          location: {
+            lat: 26.1296413,
+            lng: -80.3545787
+          },
+          streetAddress: "16001 W State Rd 84",
+          city: "Sunrise",
+          zipcode: 33326,
+          cat: "park"
+        },
+        {
+          name: "Vista View Park",
+          location: {
+            lat: 26.070546,
+            lng: -80.342975
+          },
+          streetAddress: "4001 SW 142nd Ave",
+          city: "Davie",
+          zipcode: 33330,
+          cat: "park"
+        },
+         {
+            name: 'Graciano',
+            location: {
+                lat: 26.097359,
+                lng: -80.381417
+            },
+            streetAddress: "1721 Main St",
+            city: "Weston",
+            zipcode: 33326,
+            cat: "food"
+        },
+        {
+          name: "Vienna Cafe",
+          location: {
+            lat: 26.1033643,
+            lng: -80.2706109
+          },
+          streetAddress: "9100 W State Rd",
+          city: "Davie",
+          zipcode: 33324,
+          cat: "food"
+        },
+        {
+          name: "Sawgrass Mills Mall",
+          location: {
+            lat: 26.150166,
+            lng: -80.324821
+          },
+          streetAddress: "Sawgrass Mills Cir",
+          city: "Sunrise",
+          zipcode: 33323,
+          cat: "shopping"
+        },
+        {
+          name: "Fort Lauderdale Hollywood Airport",
+          location: {
+            lat: 26.07428482,
+            lng: -80.15067101
+          },
+          streetAddress: "100 Terminal Dr",
+          city: "Fort Lauderdale",
+          zipcode: 33315,
+          cat: "airport"
+        },
+        {
+          name: "The Shops At Pembroke Gardens",
+          location: {
+            lat: 26.00470497,
+            lng: -80.33688068
+          },
+          streetAddress: "527 SW 145th Terrace",
+          city: "Pembroke Pines",
+          zipcode: 33027,
+          cat: "shopping"
+        }
+
+    ];
+
+var Location = function(data) {
+  this.name = data.name;
+  this.zipcode = data.zipcode;
+  this.location = data.location;
+  this.streetAddress = data.streetAddress;
+  this.city = data.city;
+  this.cat = data.cat;
+}
+
+var ViewModel = function() {
+  var self = this;
+  this.markers = ko.observable([]);
+  this.locationList = ko.observableArray([]);
+
+  initialLocations.forEach(function(locationItem) {
+    self.locationList.push(new Location(locationItem));
+  });
+
+  this.currentLocation = ko.observable(this.locationList()[0]);
+
+  this.setLocation = function(clickedLocation) {
+    self.currentLocation(clickedLocation);
+  };
+
+
 
 }
 
-
+initMap();
 
 ko.applyBindings(new ViewModel());
