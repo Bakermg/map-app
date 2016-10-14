b35c2b9d7b6d1f3b4146e1096d5210e46a54bb5a
Mike Baker <bakermg@me.com>
2016-10-14 10:30:26 -0400

    Fixed some comments

--- scripts/app.js
+++ scripts/app.js
@@ -78,7 +78,7 @@ var styles = [{
 }];
 
 
-// maker the marker a custom color from Udacity google maps course
+// make the marker a custom color from Udacity google maps course
 function makeMarkerIcon(markerColor) {
     var markerImage = new google.maps.MarkerImage(
         'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor + '|40|_|%E2%80%A2',
